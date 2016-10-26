39e4ae60acfce1fa9f6554470f678944ae8266dc
Bret Stateham <Bret.Stateham@microsoft.com>
2016-10-26 09:47:34 -0700

    Removed nconf, getting env variables directly

--- ingenufunctions/IntellectRest2IoTHubJs/index.js
+++ ingenufunctions/IntellectRest2IoTHubJs/index.js
@@ -8,12 +8,6 @@
 // ----------------------------------------------------------------------
 // Requires
 // ----------------------------------------------------------------------
-// nconf is used to load configuration values from the 
-// command line args, environment variables, or config file
-// GitHub: https://github.com/indexzero/nconf
-// NPM:    https://www.npmjs.com/package/nconf 
-var nconf = require('nconf');
-
 // Tedious is used to connect to the Azure SQL Database: 
 // GitHub: https://github.com/tediousjs/tedious
 // NPM:    https://www.npmjs.com/package/tedious
@@ -25,7 +19,7 @@ var Connection = require('tedious').Connection;
 // sqlConfig stores the SQL Database Connection Details
 // TODO: Move username and password info out into secure config 
 var sqlConfig = {
-    server: nconf.get("SqlServer"),
+    server: GetEnvironmentVariable("SqlServer"),
     options: {encrypt: true, database: 'ingenudb'}
 };
 
