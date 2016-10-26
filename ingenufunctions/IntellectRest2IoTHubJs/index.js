249fdd691668fb2098b5f0c934bcb872d3500828
Bret Stateham <Bret.Stateham@microsoft.com>
2016-10-26 08:24:06 -0700

    Testing

--- ingenufunctions/IntellectRest2IoTHubJs/index.js
+++ ingenufunctions/IntellectRest2IoTHubJs/index.js
@@ -1,11 +1,17 @@
 module.exports = function (context, timerTrigger) {
+    
     var timeStamp = new Date().toISOString();
     
     if(timerTrigger.isPastDue)
     {
         context.log('Node.js is running late!');
     }
+    context.log('Saying Hi: ' + sayHi());
     context.log('Node.js timer trigger function ran!', timeStamp);   
     
     context.done();
 };
+
+var sayHi = function(){
+    return "Hi";
+}
