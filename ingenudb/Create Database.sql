8fc9c0d69ad5ff13bb5f7864b572ff287a3de5e8
Bret Stateham <Bret.Stateham@microsoft.com>
2016-10-26 12:34:10 -0700

    Working on SQL connectivity

--- ingenudb/Create Database.sql
+++ ingenudb/Create Database.sql
@@ -24,12 +24,34 @@ To populate this table:
 1. Retrieve the primary connection string for the new Azure IoT Hub device identity
 1. Using the NodeId/deviceId and primary connection strings from above, insert a new record in the table.  
 
-Sample Insert:
+Sample Insert and Query:
 
 insert into dbo.IoTDevices (deviceId,primaryConnectionString) VALUES (
   'paste your Intellect NodeID / Azure IoT Hub devicId here',
   'paste your Azure IoT Hub primary connection string here'
 );
+go
+select * from dbo.IoTDevices;
 
 */
+go
+if exists(select object_id from sys.objects where type='u' and name='LastSDUs')
+  begin
+    print 'Dropping existing dbo.LastSDUs table...'
+    drop table dbo.LastSDUs;
+  end
+go
+print 'Creating dbo.LastSDUs table...'
+go
+CREATE TABLE dbo.LastSDUs 
+(
+  readerId nvarchar(128) NOT NULL
+    CONSTRAINT PK_LastSDUs_readerId
+    PRIMARY KEY CLUSTERED,
+  lastSDU nvarchar(512) NOT NULL  
+)
+go
+select * from dbo.LastSDUs;
+go
+
 
--- ingenufunctions/IntellectRest2IoTHubJs/index.js
+++ ingenufunctions/IntellectRest2IoTHubJs/index.js
@@ -13,7 +13,7 @@
   GitHub: https://github.com/tediousjs/tedious
   NPM:    https://www.npmjs.com/package/tedious
 */
-var Connection = require('tedious').Connection; 
+var Connection = require('tedious').Connection;
 
 // ----------------------------------------------------------------------
 // Module level declarations
@@ -21,12 +21,12 @@ var Connection = require('tedious').Connection;
 
 /** sqlConfig stores the SQL Database Connection Details 
   TODO: Move username and password info out into secure config
-*/ 
+*/
 var sqlConfig = {
-    userName: GetEnvironmentVariable("SqlLogin"),  
-    password: GetEnvironmentVariable("SqlPassword"),  
+    userName: GetEnvironmentVariable("SqlLogin"),
+    password: GetEnvironmentVariable("SqlPassword"),
     server: GetEnvironmentVariable("SqlServer"),
-    options: {encrypt: true, database: GetEnvironmentVariable("SqlDb")}
+    options: { encrypt: true, database: GetEnvironmentVariable("SqlDb") }
 };
 
 // ----------------------------------------------------------------------
@@ -34,13 +34,63 @@ var sqlConfig = {
 // ----------------------------------------------------------------------
 
 /** Retrieve's an Azure IoT Hub Device Connection String from the SQL Database assuming it exists.  ' */
-function getDeviceConnectionStringFromSQL(deviceId){
+function getDeviceConnectionStringFromSQL(deviceId) {
+    var result = runQuery("SELECT primaryConnectionString from dbo.IoTHubDevices WHERE deviceId = " + deviceId);
+    console.log("Result: ");
+    console.log(result);
+    return result;
+}
+
+function runQuery(query) {
+    var result = {};
+
+    var connection = new Connection(sqlConfig);
+    connection.on('connect', function (err) {
+
+        if (err) {
+            console.error("There was an error connecting to the database: " + err);
+            result.error = err;
+            return result;
+        }
+        // If no error, then good to proceed.  
+        console.log("Connected to sql database");
+
+        var sqlRequest = new Request(query,
+            function (err) {
+                if (err) {
+                    console.log('An error occurred when executing the sql request:\n' + err);
+                    result.error = err;
+                    return result;
+                }
+            });
+
+        sqlRequest.on('doneInProc', function (rowCount, more, rows) {
+            result.rows = rows;
+            // console.log('doneInProc: ' + rowCount + ' rows returned');
+            // console.log(rows.length);
+            // rows.forEach(function (row) {
+            //     row.forEach(function (column) {
+            //         if (column.value === null) {
+            //             console.log('NULL');
+            //         } else {
+            //             result += column.value + " ";
+            //         }
+            //     });
+            //     console.log(result);
+            //     result = "";
+            // });
+            connection.close();
+            return result;
+        });
+
+        connection.execSql(sqlRequest);
+
+    });
 
 }
 
 /** Retrieve an Environment Variable */
-function GetEnvironmentVariable(name)
-{
+function GetEnvironmentVariable(name) {
     return process.env[name];
 }
 
@@ -49,21 +99,23 @@ function GetEnvironmentVariable(name)
 // Main
 // ----------------------------------------------------------------------
 module.exports = function (context, timerTrigger) {
-    
+
     var timeStamp = new Date().toISOString();
-    
-    if(timerTrigger.isPastDue)
-    {
+
+    if (timerTrigger.isPastDue) {
         context.log('Node.js is running late!');
     }
-    
+
     var sqlServer = GetEnvironmentVariable("SqlServer");
     context.log("SqlServer Environment Variable: " + sqlServer);
     context.log("sqlConfig:");
     context.log(JSON.stringify(sqlConfig));
 
-    context.log('Node.js timer trigger function ran!', timeStamp);   
-    
+    context.log("Retrieving device connection string:");
+    getDeviceConnectionStringFromSQL(deviceId)
+
+    context.log('Node.js timer trigger function ran!', timeStamp);
+
     context.done();
 };
 
