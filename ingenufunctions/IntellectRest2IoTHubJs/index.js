a754039c10097bdfa860dec0964cb3de003ca744
Bret Stateham <Bret.Stateham@microsoft.com>
2016-10-26 17:02:40 -0700

    added hex2a

--- ingenufunctions/IntellectRest2IoTHubJs/index.js
+++ ingenufunctions/IntellectRest2IoTHubJs/index.js
@@ -181,6 +181,14 @@ function getNextUplinks(lastSDU, count, callback) {
     req.end();
 }
 
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
 function parsePayload(payload){
 
 // //var payload = "Bw0ACjc1LjIwXzE5LjAwAA==";
